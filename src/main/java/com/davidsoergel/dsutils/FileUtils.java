14d46f9cf7c2682aa32cb9bf2a14f086fb9b3c17
David Soergel <dev@davidsoergel.com>
2007-10-16 02:31:28 +0000

    complete refactoring of injection mechanism in the works; currently dealing with parameter inheritance, getting rid of all ThreadLocals in favor of Factories and setResults

--- src/main/java/com/davidsoergel/dsutils/FileUtils.java
+++ src/main/java/com/davidsoergel/dsutils/FileUtils.java
@@ -39,7 +39,10 @@ import org.apache.log4j.Logger;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FilenameFilter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author lorax
@@ -120,5 +123,87 @@ public class FileUtils
 				}
 			}
 		return (path.delete());
+		}
+
+
+	/*	public static Set<File> getFilesWithNames(String[] filenames)
+	   {
+
+	   }*/
+
+	public static List<File> getFilesWithNames(String[] filenames)
+		{
+		/*
+
+		 try
+			{
+			theActualDistanceMeasure = PluginManager
+					.getNewInstanceByName(DistanceMeasure.class, distanceMeasure);
+			}
+		catch (PluginException e)
+			{
+			throw new PropsException(e);
+			}
+
+		if (theActualDistanceMeasure == null)
+			{
+			throw new PropsException("Can't find distancemeasure measure: " + distanceMeasure);
+			}
+*/
+
+		//	outputDirectory = new File(outputDirectoryName + File.separator + getRunId());
+		//	logger.info("Writing outputs to " + outputDirectoryName);
+		//	logger.info("Found directory: " + outputDirectory);
+
+		List<File> files = new ArrayList<File>();
+		// List<File> inputFilesList = new ArrayList<File>();
+		if (filenames != null)
+			{
+			try
+				{
+				for (String f : filenames)
+					{
+					if (f.endsWith("*"))
+						{
+						logger.info("Separator: " + File.separator);
+						File dir = new File(f.substring(0, f.lastIndexOf(File.separator)));
+
+						// TODO full-blown pattern matching
+						final String prefix = f.substring(f.lastIndexOf(File.separator) + 1, f.length() - 1);
+						for (File r : dir.listFiles(new FilenameFilter()
+						{
+						public boolean accept(File file, String string)
+							{
+							return string.startsWith(prefix);
+							}
+						}))
+							{
+							files.add(r);
+							//	inputFilesList.add(r);
+							}
+						}
+					else
+						{
+						files.add(new File(f));
+						//	inputFilesList.add(new File(f));
+						}
+					}
+				}
+			catch (Exception e)
+				{
+				throw new RuntimeException("trouble listing files.", e);
+				}
+			}
+		return files;
+		}
+
+	public static File getDirectory(String name)
+		{
+		File dir = new File(name);
+		if (!dir.exists())
+			{
+			dir.mkdirs();
+			}
+		return dir;
 		}
 	}
