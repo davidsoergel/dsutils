f6f0d2c0d7996ac06e6af616c1b277be34672a0b
David Soergel <dev@davidsoergel.com>
2008-12-01 23:27:32 +0000

    SVM integration

---
+++ src/main/java/com/davidsoergel/dsutils/stringmapper/FloatMapper.java
@@ -0,0 +1,28 @@
+package com.davidsoergel.dsutils.stringmapper;
+
+import java.lang.reflect.Type;
+
+/**
+ * @author <a href="mailto:dev@davidsoergel.com">David Soergel</a>
+ * @version $Id$
+ */
+public class FloatMapper extends StringMapper<Float>
+	{
+	public Type[] basicTypes()
+		{
+		return new Type[]{
+				Float.class,
+				float.class
+		};
+		}
+
+	public Float parse(String s)
+		{
+		return new Float(s);
+		}
+
+	public String render(Float value)
+		{
+		return value.toString();
+		}
+	}
