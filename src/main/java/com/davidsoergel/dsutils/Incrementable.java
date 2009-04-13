76e40a36d46f04cc5392a92e7c57dd85f2296925
David Soergel <dev@davidsoergel.com>
2009-04-13 02:27:14 +0000

    Jandy threading

---
+++ src/main/java/com/davidsoergel/dsutils/Incrementable.java
@@ -0,0 +1,12 @@
+package com.davidsoergel.dsutils;
+
+/**
+ * @author <a href="mailto:dev@davidsoergel.com">David Soergel</a>
+ * @version $Id$
+ */
+public interface Incrementable
+	{
+	public void increment();
+
+	void setMaximum(int i);
+	}
