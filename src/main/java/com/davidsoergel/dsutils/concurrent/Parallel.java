97196120414a73fb48196bfa4db8f71efc52e84b
David Soergel <dev@davidsoergel.com>
2009-06-11 08:51:13 +0000

    SFI multithreading

--- src/main/java/com/davidsoergel/dsutils/concurrent/Parallel.java
+++ src/main/java/com/davidsoergel/dsutils/concurrent/Parallel.java
@@ -5,6 +5,7 @@ import com.davidsoergel.dsutils.collections.NextOnlyIteratorAsNormalIterator;
 import com.google.common.base.Function;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
  * @author <a href="mailto:dev@davidsoergel.com">David Soergel</a>
@@ -20,12 +21,58 @@ public class Parallel
 	public static <T> void forEach(Iterator<T> tasks, final Function<T, Void> function)
 		{
 
-		DepthFirstThreadPoolExecutor.getInstance().submitAndWaitForAll(new RunnableForEach<T>(tasks)
+		DepthFirstThreadPoolExecutor.getInstance().submitAndWaitForAll(new ForEach<T>(tasks)
 		{
 		public void performAction(final T o)
 			{
 			function.apply(o);
 			}
 		});
+		}
+
+	// unlike RunnableForEach, this one puts the Iterator.next() call within each Runnable
+	// it will iterate forever until one of the Runnables throws an exception, so it's important to throttle elsewhere, e.g. via the permits in DepthFirstTPE.
+	private abstract static class ForEach<T> implements Iterator<Runnable>
+		{
+		Iterator<T> iter;
+
+		public ForEach(final Iterator<T> tasks)
+			{
+			iter = tasks;
+			}
+
+		boolean hasNext = true;
+
+		public boolean hasNext()
+			{
+			return hasNext;
+			}
+
+		public Runnable next()
+			{
+			return new Runnable()
+			{
+			public void run()
+				{
+				T o;
+				try
+					{
+					o = iter.next();
+					}
+				catch (NoSuchElementException e)
+					{
+					hasNext = false;
+					throw e;
+					}
+				performAction(o);
+				}
+			};
+			}
+
+		public void remove()
+			{
+			}
+
+		public abstract void performAction(T o);
 		}
 	}
